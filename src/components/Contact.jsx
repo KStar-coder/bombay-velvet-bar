7c832e87836949a861337947eaa424a37df39093
KStar-coder <sharmakaustav04@gmail.com>
2025-06-29 23:56:21 +0530

    Contact Section - Completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,42 @@
-import React from 'react'
-
+import { openingHours, socials } from '../../constants/Index'
+import { useGSAP } from '@gsap/react'
+import { SplitText } from 'gsap/all'
+import gsap from 'gsap'
 const Contact = () => {
+
+    useGSAP(() => {
+        const titleSplit = SplitText.create('#contact h2', { type: 'word' });
+
+        const timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#contact',
+                start: 'top center',
+            },
+            ease: 'power1.inOut'
+        })
+
+        timeline
+            .from(titleSplit.words, {
+                opacity: 0, yPercent: 100, stagger: 0.02
+            })
+            .from('#contact h3, #contact p', {
+                opacity: 0,
+                yPercent: 100,
+                stagger: 0.02
+            })
+            .to('#f-right-leaf', {
+                y: '-50',
+                duration: 1,
+                ease: 'power1.inOut'
+            })
+            .to('#f-left-leaf', {
+                y: '-50',
+                duration: 1,
+                ease: 'power1.inOut'
+            }, '<')
+
+    })
+
     return (
         <footer id='contact'>
             <img src="/images/footer-right-leaf.png" alt="leaf-right" id='f-right-leaf' />
@@ -8,6 +44,47 @@ const Contact = () => {
 
             <div className="content">
                 <h2>Where to Find Us</h2>
+
+                <div>
+                    <h3>Visit Out Bar</h3>
+                    <p>Anjuna Beach, Goa</p>
+                </div>
+
+                <div>
+                    <h3>Contact Us</h3>
+                    <p>+91 050607010212</p>
+                    <p>bar@bombayvelet.com</p>
+                </div>
+
+                <div>
+                    <h3>Open Everyday</h3>
+                    {openingHours.map((time) => {
+                        return (
+                            <p key={time.day}>
+                                {time.day} : {time.time}
+                            </p>
+                        )
+                    })}
+                </div>
+
+                <div>
+                    <h3>Socials</h3>
+
+                    <div className='flex-center gap-5'>
+                        {socials.map((social) => {
+                            return (
+
+                                <a key={social.name} href='social.url' target='_blank' rel='noopener norefer'
+                                    aria-label={social.name}
+                                >
+                                    <img src={social.icon} />
+                                </a>
+                            )
+                        })}
+                    </div>
+
+                </div>
+
             </div>
 
         </footer>
